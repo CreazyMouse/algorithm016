55cbd39ba8a86aeeaa54dfcf4691074ca7e3175f
CreazyMouse <384679071@qq.com>
2020-11-09 12:48:45 +0800

    修改2的幂中的内容

--- Week_08/2的幂.cpp
+++ Week_08/2的幂.cpp
@@ -1,12 +1,17 @@
 class Solution {
 public:
-    int hammingWeight(uint32_t n) {
-        //按位与计算
-        int num=0;
-        while(n != 0) {
-            num++;
-            n &= (n-1);
-        }
-        return num;
+    bool isPowerOfTwo(int n) {
+        //1.循环取2的余数，然后除以2
+        //  if (n == 0) 
+        //     return false;
+        //  while (n % 2 == 0)
+        //   n /= 2;
+        // return n == 1;
+        //2.位运算，2的幂次方中，该数的二进制中仅能有1位为1
+        if(0 == n)
+            return false;
+        long x = (long)n;
+        return (x & (x-1)) == 0;
+
     }
 };
